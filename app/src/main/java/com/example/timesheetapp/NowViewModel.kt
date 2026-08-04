0ffcb80a25125b86db364ed74d3069be608852b0
2008oxfordcomma <2008oxfordcomma@gmail.com>
2026-08-04 09:33:19 -0500

    Added some basic function and logic for clock in / clock out

--- app/src/main/java/com/example/timesheetapp/NowViewModel.kt
+++ app/src/main/java/com/example/timesheetapp/NowViewModel.kt
@@ -2,11 +2,59 @@ package com.example.timesheetapp
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import java.time.LocalTime
 
 class NowViewModel(app: Application): AndroidViewModel(app) {
-//    TODO private member variables
+    //    TODO private member variables
+    private val _isClockedIn = MutableStateFlow(false)
+    private val _clockInTime = MutableStateFlow<String?>(null)
+    private val _clockOutTime = MutableStateFlow<String?>(null)
+    private val historyViewModel = HistoryLogViewModel(app)
+    private var currentWork: Work? = null
 
-//    TODO init block that initalize seriablize data to store
+    val isClockedIn: StateFlow<Boolean> = _isClockedIn
+    val clockInTime: StateFlow<String?> = _clockInTime
+    val clockOutTime: StateFlow<String?> = _clockOutTime
 
-//    TODO functions that change private member variables
+
+
+    //    TODO init block that initalize seriablize data to store
+
+    //    TODO functions that change private member variables
+    fun updateLocationStatus(isNear: Boolean) {
+        if (isNear && !_isClockedIn.value) clockIn()
+        else if (!isNear && _isClockedIn.value) clockOut()
+    }
+
+    private fun clockIn() {
+        val currentTime = LocalTime.now()
+        _isClockedIn.value = true
+        _clockInTime.value = currentTime.toString()
+
+        currentWork = Work(clockIn = currentTime)
+        historyViewModel.add(clockIn = currentTime)
+    }
+
+    private fun clockOut() {
+        val currentTime = LocalTime.now()
+        _isClockedIn.value = false
+        _clockOutTime.value = currentTime.toString()
+
+        currentWork?.let { work ->
+            val updatedWork = Work(
+                date = work.date,
+                clockIn = work.clockIn,
+                clockOut = currentTime
+            )
+            historyViewModel.remove(work)
+            historyViewModel.add(clockIn = work.clockIn, newWork = updatedWork)
+            currentWork = null
+        }
+    }
+
+    fun getHistory(): List<Work> {
+        return historyViewModel.getLog()
+    }
 }
