45e4c1c6c218b6cfff718c008aac58aae0e72aa6
Dre Harm <2008oxfordcomma@gmail.com>
2026-08-05 12:45:05 -0500

    Add documentation references in NominatimAPI.kt
    Added references to Nominatim API documentation.

--- app/src/main/java/com/example/timesheetapp/data/remote/NominatimAPI.kt
+++ app/src/main/java/com/example/timesheetapp/data/remote/NominatimAPI.kt
@@ -2,6 +2,10 @@ package com.example.timesheetapp.data.remote
 
 
 // pulled straight from the API page
+/* references 
+* https://nominatim.org/release-docs/develop/library/Getting-Started/
+* https://nominatim.org/release-docs/develop/api/Overview/
+*/
 
 import com.example.timesheetapp.data.model.AddressResult
 import retrofit2.http.GET
