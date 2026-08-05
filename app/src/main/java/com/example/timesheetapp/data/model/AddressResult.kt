b74b735894496a177c95f91611f5a107dfa1f56c
Dre Harm <2008oxfordcomma@gmail.com>
2026-08-05 12:43:58 -0500

    Added references

--- app/src/main/java/com/example/timesheetapp/data/model/AddressResult.kt
+++ app/src/main/java/com/example/timesheetapp/data/model/AddressResult.kt
@@ -1,5 +1,10 @@
 package com.example.timesheetapp.data.model
 
+/* references 
+* https://nominatim.org/release-docs/develop/library/Getting-Started/
+* https://nominatim.org/release-docs/develop/api/Overview/
+*/
+
 data class AddressResult(
     // We can't change this names since the API says these are the variable names it's expecting
     val display_name: String,
